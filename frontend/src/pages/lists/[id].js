10e2a0f7f31538e5df7a9887b3e774a6d1f6e3f6
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-25 15:48:42 +0900

    docs:一部削除

--- frontend/src/pages/lists/[id].js
+++ frontend/src/pages/lists/[id].js
@@ -1,12 +1,12 @@
 // 詳細
 import Link from 'next/link';
-import {getStaticProps} from '../index'
+
 
 export default function DiaryDetail(){
   return(
     <div>
       <h1>詳細表示</h1>
-    <Link href={`/lists/[id]/[title]`}><button>詳細</button></Link>
+    <Link href={`/lists/[id]/[title]`}><button>編集</button></Link>
     </div>
 
   )
