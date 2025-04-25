3130e08ec039874c6fecdb29ceaf5351fc68c059
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-25 14:52:33 +0900

    docs:Axiosをインストール

--- frontend/src/pages/api/axiosInstance.js
+++ frontend/src/pages/api/axiosInstance.js
@@ -1,1 +1,12 @@
 // API通信設定
+import axios from "axios";
+
+//バックエンドとのAPI通信
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3001/api", 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
