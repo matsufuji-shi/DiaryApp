42d527d3fd0be143b7f12a5d2d1511064c4b208a
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-25 16:22:13 +0900

    docs:一覧表示

---
+++ frontend/src/pages/DiaryForm.js
@@ -0,0 +1,5 @@
+export default function Form(){
+  return(
+    <h1>編集と追加</h1>
+  )
+}
--- frontend/src/pages/lists/[id].js
+++ frontend/src/pages/lists/[id].js
@@ -6,7 +6,7 @@ export default function DiaryDetail(){
   return(
     <div>
       <h1>詳細表示</h1>
-    <Link href={`/lists/[id]/[title]`}><button>編集</button></Link>
+    <Link href={`/DiaryForm`}><button>編集</button></Link>
     </div>
 
   )
